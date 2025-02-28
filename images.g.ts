8e8d11d865ae180cdca7a4110650f9ebde1f5d85
The2ndAmendment2 <479869@bsd48.org>
2025-02-28 13:30:17 -0800

    Update pxt.json, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,48 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myHand":return img`
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+.......................eeeee....
+.......................eeeeee...
+.......................eeeeeee..
+.......................eeeeeeee.
+.......................eeeeeeee9
+........................eeeeeee9
+.........................eeeeee9
+..........................eeeee9
+...........................eee99
+............................e999
+.............................999
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
